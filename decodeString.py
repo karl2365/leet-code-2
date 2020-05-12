b271fcb24792666fcbdaf927c877e5c26a9ddd19
karl2365 <karlbeksa@gmail.com>
2020-05-12 15:06:18 -0400

    decode string

---
+++ decodeString.py
@@ -0,0 +1,29 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        stack = []
+        mult = 0
+        currStr = ""
+        
+        #check each character is input string
+        for char in s:
+            if char == '[':
+                stack.append(currStr)
+                stack.append(mult)
+                currStr = ""
+                mult = 0
+                
+            elif char == ']':
+                count = stack.pop()
+                prevStr = stack.pop()
+                currStr = prevStr + currStr * count 
+            
+            #allow for multi-digit numbers for the multiplier
+            elif char.isdigit():
+                mult = mult *10 + int(char)
+                
+            else: #char is alpha by default
+            
+                currStr += char
+                
+        return currStr
